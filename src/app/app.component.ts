3d0b427b501111b370d2277e4a85d4efbd237a92
roody95 <agbassahr@gmail.com>
2020-12-13 17:16:20 +0000

    cmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ultra';
+  boot = false;
+
+
+  ngOnInit(): void {
+
+    setTimeout(() => {
+      this.boot = true;
+    }, 500);
+
+  }
 }
