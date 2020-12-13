1949a3957e9ebd5efcdbd6cc1d9bbe5aa4012306
roody95 <agbassahr@gmail.com>
2020-12-13 17:09:17 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    LayoutComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
