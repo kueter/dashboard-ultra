0750ef3518f7656ee4a17ae009b4210024ffc736
roody95 <agbassahr@gmail.com>
2020-12-18 15:43:34 +0000

    commit

---
+++ src/app/layout/elements/widgets-ui/widgets-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-widgets-ui',
+  templateUrl: './widgets-ui.component.html',
+  styleUrls: ['./widgets-ui.component.scss']
+})
+export class WidgetsUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/layout/elements/widgets-ui/widgets-ui.module.ts
+++ src/app/layout/elements/widgets-ui/widgets-ui.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { WidgetsUiComponent } from './widgets-ui.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  component: WidgetsUiComponent }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [WidgetsUiComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
