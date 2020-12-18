ce73b22ed0ff18d86ad0345cf82843679a7adbe7
roody95 <agbassahr@gmail.com>
2020-12-18 14:07:09 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
@@ -18,7 +19,8 @@ import { LoadingComponent } from './loading/loading.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,18 +1,49 @@
+import { query, style, animate, group  } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+const left = [
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  group([
+      query(':enter', [style({ transform: 'translateX(-100%)' }), animate('.4s ease-out', style({ transform: 'translateX(0%)' }))], {
+          optional: true,
+      }),
+      query(':leave', [style({ transform: 'translateX(0%)' }), animate('.4s ease-out', style({ transform: 'translateX(100%)' }))], {
+          optional: true,
+      }),
+  ]),
+];
+
+const right = [
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  group([
+      query(':enter', [style({ transform: 'translateX(100%)' }), animate('.4s ease-out', style({ transform: 'translateX(0%)' }))], {
+          optional: true,
+      }),
+      query(':leave', [style({ transform: 'translateX(0%)' }), animate('.4s ease-out', style({ transform: 'translateX(-100%)' }))], {
+          optional: true,
+      }),
+  ]),
+];
+
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  animations: []
 })
 export class LayoutComponent implements OnInit {
-  sideState: boolean;
+  sideState: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  preparedRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  }
 
   getSide(event) {
       this.sideState = event;
