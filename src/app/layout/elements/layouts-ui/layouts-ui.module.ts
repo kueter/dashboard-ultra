7d02481c199acfd4d50ad3436ff85abb4a6a1075
roody95 <agbassahr@gmail.com>
2020-12-18 16:01:00 +0000

    commit

--- src/app/layout/elements/layouts-ui/layouts-ui.module.ts
+++ src/app/layout/elements/layouts-ui/layouts-ui.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutsUiComponent } from './layouts-ui.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '',  component: LayoutsUiComponent }
+];
 
 @NgModule({
   declarations: [LayoutsUiComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class LayoutsUiModule { }
