4047c9dc37c278a13e6198bd1300f3001395885b
roody95 <agbassahr@gmail.com>
2020-12-18 15:51:58 +0000

    commit

---
+++ src/app/layout/elements/forms-ui/forms-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forms-ui',
+  templateUrl: './forms-ui.component.html',
+  styleUrls: ['./forms-ui.component.scss']
+})
+export class FormsUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/layout/elements/forms-ui/forms-ui.module.ts
+++ src/app/layout/elements/forms-ui/forms-ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsUiComponent } from './forms-ui.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FormsUiComponent],
   imports: [
     CommonModule
   ]
