09f429121fc05e5cb0637517fcae0d4023a589f1
roody95 <agbassahr@gmail.com>
2021-01-03 10:24:32 +0000

    commit

--- src/app/layout/elements/forms-ui/forms-ui.module.ts
+++ src/app/layout/elements/forms-ui/forms-ui.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsUiComponent } from './forms-ui.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
   { path: '' , component: FormsUiComponent }
@@ -11,7 +12,8 @@ const routes: Routes = [
   declarations: [FormsUiComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ]
 })
 export class FormsUiModule { }
--- src/app/layout/elements/widgets-ui/widgets-ui.module.ts
+++ src/app/layout/elements/widgets-ui/widgets-ui.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { WidgetsUiComponent } from './widgets-ui.component';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
   { path: '',  component: WidgetsUiComponent }
@@ -11,7 +12,8 @@ const routes: Routes = [
   declarations: [WidgetsUiComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ]
 })
 export class WidgetsUiModule { }
