99853e00a58763e28c1c51e1cf0c65ce19014d34
roody95 <agbassahr@gmail.com>
2021-01-05 17:57:24 +0000

    commit

--- src/app/layout/elements/shared/shared.module.ts
+++ src/app/layout/elements/shared/shared.module.ts
@@ -2,17 +2,39 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import {
+  PerfectScrollbarModule,
+  PERFECT_SCROLLBAR_CONFIG,
+  PerfectScrollbarConfigInterface,
+} from 'ngx-perfect-scrollbar';
+
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true,
+};
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PerfectScrollbarModule,
   ],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      // DROPZONE_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG,
+      // DEFAULT_DROPZONE_CONFIG,
+    },
+  ]
+  ,
   exports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PerfectScrollbarModule,
   ]
 })
 export class SharedModule { }
