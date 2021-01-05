571f4251ba2141e5c5b45a50a92abac2973c08a6
roody95 <agbassahr@gmail.com>
2021-01-05 21:47:54 +0000

    commit

--- src/app/layout/elements/widgets-ui/widgets-ui.component.ts
+++ src/app/layout/elements/widgets-ui/widgets-ui.component.ts
@@ -12,7 +12,7 @@ export class WidgetsUiComponent implements OnInit {
     { icon: 'fal fa-wifi fa-2x' ,title: 'Requests by day', detail: '', mount: '1.5k'},
     { icon: 'fal fa-users fa-2x' ,title: 'Client Flux', detail: '', mount: '15k'},
     { icon: 'fal fa-university fa-2x' ,title: 'Amount Bank', detail: '', mount: '12 M'},
-    { icon: 'fas fa-people-carry fa-2x' ,title: 'Employee', detail: '', mount: '0.5k'},
+    { icon: 'fad fa-people-carry fa-2x' ,title: 'Employee', detail: '', mount: '0.5k'},
   ];
 
   constructor(public lservice: LayoutService) { }
